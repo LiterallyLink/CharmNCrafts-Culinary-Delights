d075a5d5f5d06fbf1f07147b33de599dceebbe30
Peoples <67402756+LiterallyLink@users.noreply.github.com>
2025-11-01 11:06:01 -0700

    Initial Commit

---
+++ src/client/java/charmncrafts/delights/CharmNCraftsCulinaryDelightsDataGenerator.java
@@ -0,0 +1,11 @@
+package charmncrafts.delights;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CharmNCraftsCulinaryDelightsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
